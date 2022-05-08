57f7b960c792a65c7c365f399e7c39f7f682aeed
fajlarabbykhan <fajlarabbyk55@gmail.com>
2022-05-08 12:35:04 +0600

    done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,16 @@
-import { getAuth } from "firebase/auth"
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getAuth } from 'firebase/auth'
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA8C8NnrFY3ZA4hNKmSpVvxF4dyYLkQ4oE",
-    authDomain: "eelectronic-9f6e5.firebaseapp.com",
-    projectId: "eelectronic-9f6e5",
-    storageBucket: "eelectronic-9f6e5.appspot.com",
-    messagingSenderId: "215149169663",
-    appId: "1:215149169663:web:cca3e0ba7b5cdd34e135ca"
+    apiKey: "AIzaSyBUPWFG9ErK20ueQ0C0Pb720r-QBb-RoAE",
+    authDomain: "electronic-a910e.firebaseapp.com",
+    projectId: "electronic-a910e",
+    storageBucket: "electronic-a910e.appspot.com",
+    messagingSenderId: "925019054153",
+    appId: "1:925019054153:web:60b8eb5324289b4219df98"
 };
 
 // Initialize Firebase
